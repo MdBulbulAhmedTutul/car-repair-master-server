02bf12445ef3662ef91df57867c4cc0968d0f694
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-03 08:27:41 -0700

    create a cookie token

---
+++ _jwt_steps.js
@@ -0,0 +1,13 @@
+/**
+ * 1. install json wentoken
+ * 2. require jwt = require('jsonwebtoken');
+ * 3. jwt.sign(payload, secret, {expiresIn: '1h'})
+ * 4. client side npm install axios
+*/
+
+/**
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. local storage --> ok type (XSS)
+ * 3.  cookies: http only
+ * */ 
--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleWare
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 
@@ -37,11 +40,17 @@ async function run() {
         })
 
         // auth related api
-        app.post('/jwt', async(req, res) =>{
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log(user);
-            const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
-            res.send(token)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: 'none'
+            })
+            .send({success: true});
         })
 
 
@@ -90,9 +99,9 @@ async function run() {
         })
 
         //5 update booking data
-        app.patch('/bookings/:id', async(req, res) =>{
+        app.patch('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateBooking = req.body;
             console.log(updateBooking);
             const updateDoc = {
